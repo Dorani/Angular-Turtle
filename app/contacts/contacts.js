159ff07fd2788936f471a508109da0df1d37ba14
Seif <seif.dorani@gmail.com>
2017-02-09 13:02:28 -0500

    adding contacts to database

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -12,6 +12,22 @@ angular.module('myApp.contacts', ['ngRoute'])
 .controller('ContactsCtrl', ['$scope', '$firebaseArray',function($scope, $firebaseArray) { //allows us to bind data to and form the view from the controller
   var ref = new Firebase('https://contactlist-31b15.firebaseio.com/')//url from firebase.com database after app was created
 
+
   $scope.contacts = $firebaseArray(ref);//put all contacts in scope var to be used on view
+  $scope.addContact = function(){
+    $scope.contacts.$add({
+      name: $scope.name,
+      email: $scope.email,
+      phone: $scope.phone
+    }).then(function(ref){
+      var id = ref.key();
+      console.log("added contact" + id);
+      $scope.name = '';
+      $scope.email = '';
+      $scope.phone = '';
+    });
+  }
+
 }]);
+
 //inject firebase as well as in paramaters
