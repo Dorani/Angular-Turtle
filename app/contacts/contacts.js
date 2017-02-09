cbd9fa06da326b5959effb47d5e79b8028d786d9
Seif <seif.dorani@gmail.com>
2017-02-09 12:49:18 -0500

    html forms created changing content now

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -11,5 +11,7 @@ angular.module('myApp.contacts', ['ngRoute'])
 
 .controller('ContactsCtrl', ['$scope', '$firebaseArray',function($scope, $firebaseArray) { //allows us to bind data to and form the view from the controller
   var ref = new Firebase('https://contactlist-31b15.firebaseio.com/')//url from firebase.com database after app was created
+
+  $scope.contacts = $firebaseArray(ref);//put all contacts in scope var to be used on view
 }]);
 //inject firebase as well as in paramaters
